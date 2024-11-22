259ead8ce5176bcc80ba5481badd07bcc74c815b
Tatu Sartovuo <171710@sskky.fi>
2024-11-22 10:37:22 +0200

    osa 4 tehtävä 1 tehty

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,41 +2,24 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+.then(() => {
+  logger.info('connected to MongoDB')
+})
+.catch((error) => {
+  logger.error('error connecting to MongoDB:', error.message)
+})
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
