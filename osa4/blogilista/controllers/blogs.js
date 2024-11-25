1bb1ab9ed21184c8f103516b45c0c60f0eb972ea
tatsart23 <Sartovuo@gmail.com>
2024-11-25 20:50:55 +0200

    Jest testaukset tehty.

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -9,6 +9,48 @@ blogRouter.get("/", (request, response) => {
   });
 });
 
+// DELETE /api/blogs/:id
+blogRouter.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Blog.findByIdAndDelete(id);
+
+    if (result) {
+      res.status(204).end(); 
+    } else {
+      res.status(404).json({ error: "Blog not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" }); 
+  }
+});
+
+// PUT /api/blogs/:id
+blogRouter.put("/:id", async (req, res) => {
+  const { likes } = req.body;
+
+  const update = { likes };
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      update,
+      { new: true, runValidators: true, context: "query" }
+    );
+
+    if (updatedBlog) {
+      res.json(updatedBlog);
+    } else {
+      res.status(404).json({ error: "Blog not found" });
+    }
+  } catch (error) {
+    console.error("Error updating blog:", error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
 // POST /api/blogs
 blogRouter.post("/", (request, response) => {
   const body = request.body;
