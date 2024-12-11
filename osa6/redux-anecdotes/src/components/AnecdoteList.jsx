32feceed5c03a8ab23c641e2e6748132b327726f
tatsart23 <Sartovuo@gmail.com>
2024-12-11 11:29:09 +0200

    osa 6 tehtavat osasta 2 tehty

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,27 +1,33 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
+import { vote } from '../reducers/anecdoteReducer';
+import { showNotification } from '../reducers/notificationReducer';
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => state.anecdotes);
+  const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
-  const vote = (id) => {
-    dispatch({
-      type: "VOTE",
-      payload: { id },
-    });
+  const voteAnecdote = (id, content) => {
+    dispatch(vote(id)); 
+    dispatch(showNotification(`You voted '${content}'`, 5000)); 
   };
+
+  const filteredAnecdotes = anecdotes.filter((anecdote) =>
+    anecdote.content.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
-      {[...anecdotes]
-        .sort((a, b) => b.votes - a.votes) // Järjestetään äänten mukaan
+      {[...filteredAnecdotes]
+        .sort((a, b) => b.votes - a.votes)
         .map((anecdote) => (
           <div key={anecdote.id}>
             <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
+              <button onClick={() => voteAnecdote(anecdote.id, anecdote.content)}>
+                vote
+              </button>
             </div>
           </div>
         ))}
